42d28215ccc7934a85d455209cfe83e9d18fb442
Nicholas Evans <ntevans8@gmail.com>
2022-02-09 22:28:34 -0600

    Set Up

--- website/auth.py
+++ website/auth.py
@@ -2,14 +2,17 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+# User login page
 @auth.route('/login')
 def login():
     return "<h1>Login<h1>"
 
+# User sign up page
 @auth.route('/sign-up')
 def signup():
     return "<h1>Sign Up<h1>"
 
+# User logout function
 @auth.route('/logout')
 def logout():
     return "<h1>Logout<h1>"
--- website/views.py
+++ website/views.py
@@ -2,22 +2,27 @@ from flask import Blueprint
 
 views = Blueprint('views', __name__)
 
+# Home page
 @views.route('/')
 def index():
     return "<h1>Index<h1>"
 
+# Where users manage character sheets
 @views.route('/characters')
 def characters():
     return "<h1>Characters<h1>"
 
+# Information regarding various races
 @views.route('/races')
 def races():
     return "<h1>Races<h1>"
 
+# Information regarding various classes
 @views.route('/classes')
 def classes():
     return "<h1>Classes<h1>"
 
+# Information regarding various backgrounds
 @views.route('/backgrounds')
 def backgrounds():
     return "<h1>Backgrounds<h1>"
